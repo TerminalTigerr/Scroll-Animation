901b0eb0ef5f228279629472fb446b4efb8e4fb8
BitHero <nuhumusah874@gmail.com>
2023-05-11 08:43:45 +0000

    checkBoxes

--- script.js
+++ script.js
@@ -5,5 +5,16 @@ window.addEventListener('scroll', checkBoxes)
 checkBoxes()
 
 function checkBoxes() {
-    console.log(window.innerHeight / 5 * 4)
+    const triggerBottom = (window.innerHeight / 5 * 4)
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        }
+        else {
+            box.classList.remove('show')
+        }
+    })
 }
